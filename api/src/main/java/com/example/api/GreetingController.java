e934a7533d9f5ef1603e7a31efdb4caf90915ef1
VictorLCosta <victorlc2019@outlook.com>
2022-12-18 12:43:03 -0300

    refactor: learning to use path variables

--- api/src/main/java/com/example/api/GreetingController.java
+++ api/src/main/java/com/example/api/GreetingController.java
@@ -2,8 +2,9 @@ package com.example.api;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +13,8 @@ public class GreetingController {
     private static final String template = "Hello, %s";
     private final AtomicLong counter = new AtomicLong();
     
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    @RequestMapping(value = "/greeting/{name}", method = RequestMethod.GET)
+    public Greeting greeting(@PathVariable(value = "name") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
